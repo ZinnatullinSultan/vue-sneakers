a3d3cc66bd5212d2abda4222efcff6c0559001c9
ZinnatullinSultan <zinnatullin.969@gmail.com>
2024-06-03 13:20:26 +0300

    first commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import Card from './Card.vue'
+
+  defineProps({
+    items: Array,
+    isFavorites: Boolean
+  })
+
+  const emit = defineEmits(['addToFavorite','addToCart'])
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5" v-auto-animate>
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :id = "item.id"
+      :image-url="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
+      :on-click-favorite="isFavorites ? null : ()=>emit('addToFavorite', item)"
+      :on-click-add="isFavorites ? null : ()=>emit('addToCart', item)"
+      :is-favorite="item.isFavorite"
+      :is-added="item.isAdded"
+    />
+  </div>
+</template>
---
+++ src/pages/Favorites.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, ref} from 'vue'
+import axios from 'axios'
+import CardList from '../components/CardList.vue'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://0941c0e0b9eaddc0.mokky.dev/favorites')
+
+    favorites.value = data.map((obj) => obj.item)
+  } catch (error) {
+    console.log(error)
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
+
+  <CardList :items="favorites" is-favorites />
+</template>
